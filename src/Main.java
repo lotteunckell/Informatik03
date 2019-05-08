98e856fd4ef50bef9ad1b684ebd4f05edcfca879
lotteunckell <lotteunckell@gmail.com>
2019-05-08 10:32:15 +0200

    moved stuff out of main method

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import Fabriken.DidadasFabrik;
 import Fabriken.EnkiFabrik;
+import Fabriken.Fabrik;
 import Fabriken.UmapFabrik;
 
 public class Main {
     static public void main(String[] args) {
         System.out.println("Produziert von Didadas--------------------------------------");
-        DidadasFabrik didadasFabrik = new DidadasFabrik();
-        didadasFabrik.getInstance(37, "TollerDidadasSportschuh", 43, "TollerDidadasWanderschuh", 41, "TollerDidadasStiefel");
-        System.out.println("------------------------------------------------------------");
+        schuheHerstellen(new DidadasFabrik(), 37, "TollerDidadasSportschuh", 43, "TollerDidadasWanderschuh", 41, "TollerDidadasStiefel");
         System.out.println("Produziert von Enki-----------------------------------------");
-        EnkiFabrik enkiFabrik = new EnkiFabrik();
-        enkiFabrik.getInstance(37, "TollerEnkiSportschuh", 43, "TollerEnkiWanderschuh", 41, "TollerEnkiStiefel");
-        System.out.println("------------------------------------------------------------");
+        schuheHerstellen(new EnkiFabrik(), 37, "TollerEnkiSportschuh", 43, "TollerEnkiWanderschuh", 41, "TollerEnkiStiefel");
         System.out.println("Produziert von Umap-----------------------------------------");
-        UmapFabrik umapFabrik = new UmapFabrik();
-        umapFabrik.getInstance(37, "TollerUmapSportschuh", 43, "TollerUmapWanderschuh", 41, "TollerUmapStiefel");
-        System.out.println("------------------------------------------------------------");
+        schuheHerstellen(new UmapFabrik(), 37, "TollerUmapSportschuh", 43, "TollerUmapWanderschuh", 41, "TollerUmapStiefel");
+    }
+
+    static void schuheHerstellen(Fabrik fabrik, int sportSchuhSize, String sportSchuhName, int wanderSchuhSize, String wanderSchuhName, int stiefelSize, String stiefelName) {
+        fabrik.getInstance(sportSchuhSize, sportSchuhName, wanderSchuhSize, wanderSchuhName, stiefelSize, stiefelName);
+        System.out.println("-------------------------------------------------------------");
     }
 }
